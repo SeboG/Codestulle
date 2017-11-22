2141126938f125a7d8a9f8ac1df646d8729f78d5
Chris-PC\Chris <janchristianhopp@gmail.com>
2017-11-22 11:10:31 +0100

    dyfgsdfg

---
+++ src/stull/Stulle.java
@@ -0,0 +1,12 @@
+package stull;
+
+public class Stulle
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Ich bin eine Stulle aus COde");
+	}
+
+}

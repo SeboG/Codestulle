e52bbf8140701fdb5a74af1efd504b4c8645256e
Chris-PC\Chris <janchristianhopp@gmail.com>
2017-11-23 11:59:27 +0100

    Minor Text fixes

--- src/stull/Stulle.java
+++ src/stull/Stulle.java
@@ -6,7 +6,7 @@ public class Stulle
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		System.out.println("Ich bin eine Stulle aus COde");
+		System.out.println("Ich bin eine Stulle aus Code.");
 		System.out.println("Also bei mir läuft der Code raus.");
 	}
 

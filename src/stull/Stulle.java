22949465efc54d32ca9eeaa4d31906a5301530ab
Chris-PC\Chris <janchristianhopp@gmail.com>
2017-11-23 12:05:30 +0100

    Branch mit Stullenmethode

--- src/stull/Stulle.java
+++ src/stull/Stulle.java
@@ -1,13 +1,18 @@
 package stull;
 
 public class Stulle
-{
+{	
+	public void schmieren(String streichkram) {
+		System.out.println("Die Stulle wurde mit "+ streichkram + " beschmiert.");
+	}
 
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
 		System.out.println("Ich bin eine Stulle aus Code.");
 		System.out.println("Also bei mir läuft der Code raus.");
+		Stulle stulli = new Stulle();
+		stulli.schmieren("Mettwurst");
 	}
 
 }
